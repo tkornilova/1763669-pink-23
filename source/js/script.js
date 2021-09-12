a53ea2c95e8a0d442920a831aef6f5827a2b0504
tkornilova <kornilova.tat07@gmail.com>
2021-09-11 19:37:08 -0600

    Исправления открывающегося меню

--- source/js/script.js
+++ source/js/script.js
@@ -1,12 +1,12 @@
-var navMain = document.querySelector('.page-header__wrapper');
+var header = document.querySelector('.page-header');
 var btnOpenClose = document.querySelector('.page-header__nav-button');
 
 btnOpenClose.addEventListener('click', function() {
-  if (navMain.classList.contains('page-header__wrapper--closed')) {
-    navMain.classList.remove('page-header__wrapper--closed');
-    navMain.classList.add('page-header__wrapper--opened');
+  if (header.classList.contains('page-header--closed')) {
+    header.classList.remove('page-header--closed');
+    header.classList.add('page-header--opened');
   } else {
-    navMain.classList.remove('page-header__wrapper--opened');
-    navMain.classList.add('page-header__wrapper--closed');
+    header.classList.remove('page-header--opened');
+    header.classList.add('page-header--closed');
   }
 });
