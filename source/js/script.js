92d7d147b9dde75d99f51b6cf21aa1da66b01bdc
tkornilova <kornilova.tat07@gmail.com>
2021-09-13 22:17:52 -0600

    Б30 Исправление JS

--- source/js/script.js
+++ source/js/script.js
@@ -1,6 +1,8 @@
 var header = document.querySelector('.page-header');
 var btnOpenClose = document.querySelector('.page-header__nav-button');
 
+header.classList.remove('page-header-no-js');
+
 btnOpenClose.addEventListener('click', function() {
   if (header.classList.contains('page-header--closed')) {
     header.classList.remove('page-header--closed');
